06b7cda7fce47c165575376eb92caa6fcfcf6d0b
avinash.kumar3 <avinash.kumar@zaggle.in>
2024-08-01 00:50:24 +0530

    comma seperated string -ve numbers addition using custom exception class

--- app/helpers/my_string_helper.rb
+++ app/helpers/my_string_helper.rb
@@ -1,11 +1,19 @@
 module MyStringHelper
+  class NegativeNumberError < StandardError
+    def initialize(negative_numbers)
+      super("Negative numbers are not allowed: #{negative_numbers.join(', ')}")
+    end
+  end
+
   def self.add(string)
     numbers = string.scan(/-?\d+/).map(&:to_i)
     negative_numbers = numbers.select { |n| n < 0 }
-    raise "Negative numbers not allowed: #{negative_numbers.join(', ')}" if negative_numbers.present?
+    unless negative_numbers.empty?
+      raise NegativeNumberError.new(negative_numbers)
+    end
     numbers.select { |n| n >= 0 }.sum
-  rescue StandardError => e
+  rescue NegativeNumberError => e
     puts e.message
   end
 end
-# MyStringHelper.add('//;\n1;2,5,8,9')
+# MyStringHelper.add("1,2,3,-4,-5")
