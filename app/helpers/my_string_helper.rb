bd80bffbc5ca00918823c5323bb3432d47482482
avinash.kumar3 <avinash.kumar@zaggle.in>
2024-08-01 00:44:34 +0530

    comma seperated string -ve numbers addition

--- app/helpers/my_string_helper.rb
+++ app/helpers/my_string_helper.rb
@@ -1,7 +1,11 @@
 module MyStringHelper
   def self.add(string)
-    string = string.gsub(/\D/, '')
-    string.split('').map(&:to_i).inject(:+) || 0
+    numbers = string.scan(/-?\d+/).map(&:to_i)
+    negative_numbers = numbers.select { |n| n < 0 }
+    raise "Negative numbers not allowed: #{negative_numbers.join(', ')}" if negative_numbers.present?
+    numbers.select { |n| n >= 0 }.inject(:+) || 0
+  rescue StandardError => e
+    puts e.message
   end
 end
-# MyStringHelper.add(//;\n1;2)
+# MyStringHelper.add('//;\n1;2,-5,8,-9')
