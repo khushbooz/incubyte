a5079412d88456ddb9f253af5ca0fa53d037df3a
avinash.kumar3 <avinash.kumar@zaggle.in>
2024-08-01 00:25:04 +0530

    mixed comma seperated string addition

--- app/helpers/my_string_helper.rb
+++ app/helpers/my_string_helper.rb
@@ -1,6 +1,7 @@
 module MyStringHelper
   def self.add(string)
-    string.present? ? string.split(',').map(&:to_i).inject(:+) : 0
+    string = string.gsub(/\s+/, '')
+    string.split(',').map(&:to_i).inject(:+) || 0
   end
 end
-# MyStringHelper.add('3,5')
+# MyStringHelper.add('1\,3')
